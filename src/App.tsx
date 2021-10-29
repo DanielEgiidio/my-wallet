77f1afd21b426267128234324f6e12db1a12e233
DanielEgidiio <danielegidioti@gmail.com>
2021-10-29 16:16:01 -0300

    Adição de nova instancia

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,43 @@ import { items } from './data/items';
 import { getCurrentMonth, filterListByMonth } from './helpers/dateFilter';
 import { TableArea } from './components/TableArea';
 import {InfoArea} from './components/InfoArea';
+import { InputArea } from './components/InputArea';
 
 const App = () => {
 
   const [list, setList] = useState(items);
   const [filteredList, setFilteredList] = useState<Item[]>([]);
   const [currentMonth, setCurrentMonth] = useState(getCurrentMonth);
+  const [income, setIncome] = useState(0);
+  const [expense, setExpense] = useState(0);
 
   useEffect(()=>{
     setFilteredList(filterListByMonth(list, currentMonth));
-  },[list, currentMonth])
+  },[list, currentMonth]);
 
+  useEffect(() => {
+    let incomeCount = 0;
+    let expenseCount = 0;
+
+    for (let i in filteredList) {
+      if(categories[filteredList[i].category].expense) {
+        expenseCount += filteredList[i].value;
+      } else {
+        incomeCount += filteredList[i].value;
+      }
+    }
+
+    setIncome(incomeCount);
+    setExpense(expenseCount);
+
+  },[filteredList])
+
+  const handleAddItem = (item: Item) => {
+    let newList = [...list];
+    newList.push(item);
+    setList(newList);
+  }
+  
   const handleMonthChange = (newMonth: string) => {
     setCurrentMonth(newMonth);
   }
@@ -31,8 +57,12 @@ const App = () => {
         <InfoArea 
           currentMonth={currentMonth}
           onMonthChange={handleMonthChange}
+          income={income}
+          expense={expense}
         />
-        {/* Area de Inserção */}
+
+        <InputArea onAdd={handleAddItem} />
+
         <TableArea list={filteredList}/>
       </C.Body>
     </C.Container>
--- src/components/InfoArea/index.tsx
+++ src/components/InfoArea/index.tsx
@@ -1,12 +1,15 @@
 import * as C from './styles';
 import { formatCurrentMonth } from '../../helpers/dateFilter';
+import {ResumeItem} from '../ResumeItem';
 
 type Props = {
     currentMonth: string;
     onMonthChange: (newMonth: string) => void;
+    income: number;
+    expense: number;
 }
 
-export const InfoArea = ({ currentMonth, onMonthChange }:Props) => {
+export const InfoArea = ({ currentMonth, onMonthChange, income, expense }:Props) => {
 
     const handlePrevMonth = () => {
         let [year, month] = currentMonth.split('-');
@@ -30,7 +33,13 @@ export const InfoArea = ({ currentMonth, onMonthChange }:Props) => {
                 <C.MonthArrow onClick={handleNextMonth}> ➡️ </C.MonthArrow>
             </C.MonthArea>
             <C.ResumeArea>
-
+                <ResumeItem title="Receitas" value={income} />
+                <ResumeItem title="Despesas" value={expense} />
+                <ResumeItem
+                    title="Balanço"
+                    value={income - expense}
+                    color={(income-expense) < 0 ? 'red' : 'green'}
+                />
             </C.ResumeArea>
         </C.Container>
     );
